d7349c346f1e683b6b81b2a878328b049b4d872e
willowweevil <ivanivanov@vivaldi.net>
2025-12-09 09:31:37 +0700

    add metrics labels

--- internal/exporter/labels.go
+++ internal/exporter/labels.go
@@ -4,10 +4,13 @@ package exporter
 // Task is a monitoring task created in Ping-Admin.com
 // MP (Monitoring Point) is a monitoring point for each task (tm in API response)
 const (
-	LabelTaskID   = "task_id"
-	LabelTaskName = "task_name"
-	LabelMPID     = "mp_id"
-	LabelMPName   = "mp_name"
-	LabelMPIP     = "mp_ip"
-	LabelMPGPS    = "mp_gps"
+	LabelErrorModule  = "error_module"
+	LabelErrorType    = "error_type"
+	LabelExporterType = "exporter_type"
+	LabelTaskID       = "task_id"
+	LabelTaskName     = "task_name"
+	LabelMPID         = "mp_id"
+	LabelMPName       = "mp_name"
+	LabelMPIP         = "mp_ip"
+	LabelMPGPS        = "mp_gps"
 )
